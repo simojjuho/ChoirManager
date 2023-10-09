a2e12d4bccb43ddb8a5b01f390a5cc3d2d29853d
Juho Simojoki <juho.simojoki@outlook.com>
2023-10-09 15:58:44 +0300

    Repo interfaces first round

--- ChoirManager.Core/src/CoreEntities/Choir.cs
+++ ChoirManager.Core/src/CoreEntities/Choir.cs
@@ -7,7 +7,7 @@ public class Choir : IChoir
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Location { get; set; }
-    public DateTime FoundedAt { get; set; }
+    public DateOnly FoundedAt { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset UpdatedAt { get; set; }
 }
