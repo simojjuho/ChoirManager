62e484874351db392f46e7a4b6d5aeedf95df2b2
Juho Simojoki <juho.simojoki@outlook.com>
2023-10-26 12:57:07 +0300

    Added ChoirRepository interface and edited the BaseInterface as well as ChoirQuery class

--- ChoirManager.Core/src/Abstractions/Repositories/IBaseRepository.cs
+++ ChoirManager.Core/src/Abstractions/Repositories/IBaseRepository.cs
@@ -1,14 +1,15 @@
 using System;
+using ChoirManager.Core.Abstractions.CoreEntities;
 using ChoirManager.Core.Abstractions.QueryOptions;
 
 namespace ChoirManager.Core.Abstractions.Repositories;
 
 public interface IBaseRepository<T>
 {
-    Task<T> GetAllAsync(IQueryOptions queryOptions);
-    Task<T> GetOneAsync(string altKey);
+    Task<T[]> GetAllAsync(IChoirQueryOptions queryOptions);
+    Task<T?> GetOneAsync(string altKey);
     Task<T> GetOneByIdAsync(Guid id);
     Task<T> CreateOneAsync(T entity);
     Task<T> UpdateAsync(T entity);
-    Task<T> RemoveAsync(T entity);
+    Task<bool> RemoveAsync(T entity);
 }
