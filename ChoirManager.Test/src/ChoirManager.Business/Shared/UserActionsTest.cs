ecfe4fe25b2016f1734103ac273dd7ebf2855af6
Juho Simojoki <juho.simojoki@outlook.com>
2024-02-08 15:13:52 +0200

    First test test

--- ChoirManager.Test/src/ChoirManager.Business/Shared/UserActionsTest.cs
+++ ChoirManager.Test/src/ChoirManager.Business/Shared/UserActionsTest.cs
@@ -3,5 +3,9 @@ namespace ChoirManager.Test.ChoirManager.Business.Shared;
 public class UserActionsTest
 {
     [Fact]
-    
+    public void Test_SettingUp()
+    {
+        var unit = 1;
+        Assert.Equal(1, unit);
+    }
 }
