f3f5e771344543492a1198a91372671774f36ace
Juho Simojoki <85196690+simojjuho@users.noreply.github.com>
2024-02-09 21:34:20 +0200

    Testing setup (#20)
    * Setting up test project
    
    * First file in test
    
    * First test test
    
    * First tests

---
+++ ChoirManager.Test/src/ChoirManager.Business/Shared/UserActionsTest.cs
@@ -0,0 +1,11 @@
+namespace ChoirManager.Test.ChoirManager.Business.Shared;
+
+public class UserActionsTest
+{
+    [Fact]
+    public void Test_SettingUp()
+    {
+        var unit = 1;
+        Assert.Equal(1, unit);
+    }
+}
